0082978fa6c017c57f2ec6f15c0c94ccd90d10fd
liangguifeng <1476982312@qq.com>
2022-06-20 11:06:44 +0800

    feat: Add redis config to yaml

--- go.mod
+++ go.mod
@@ -9,3 +9,11 @@ require (
 	google.golang.org/grpc v1.46.2
 	google.golang.org/protobuf v1.28.0
 )
+
+require (
+	github.com/go-redis/redis/extra/redisotel v0.3.0
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/golang/protobuf v1.5.2
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
--- internal/data/data.go
+++ internal/data/data.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-redis/redis/extra/redisotel"
+	"github.com/go-redis/redis/v8"
 	"github.com/google/wire"
 	"realworld-go-kratos/internal/conf"
 )
@@ -11,13 +13,29 @@ var ProviderSet = wire.NewSet(NewData, NewRealWorldRepository)
 
 // Data .
 type Data struct {
-	// TODO wrapped database client
+	rdb *redis.Client
 }
 
 // NewData .
-func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
-	cleanup := func() {
-		log.NewHelper(logger).Info("closing the data resources")
+func NewData(conf *conf.Data, logger log.Logger) (*Data, func(), error) {
+	hlog := log.NewHelper(logger)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:         conf.Redis.Addr,
+		Password:     conf.Redis.Password,
+		DB:           int(conf.Redis.Db),
+		DialTimeout:  conf.Redis.DialTimeout.AsDuration(),
+		WriteTimeout: conf.Redis.WriteTimeout.AsDuration(),
+		ReadTimeout:  conf.Redis.ReadTimeout.AsDuration(),
+	})
+	rdb.AddHook(redisotel.TracingHook{})
+	d := &Data{
+		rdb: rdb,
 	}
-	return &Data{}, cleanup, nil
+	return d, func() {
+		hlog.Info("message", "closing the data resources")
+		if err := d.rdb.Close(); err != nil {
+			hlog.Error(err)
+		}
+	}, nil
 }
