f3abc6f3f2e880bdf050541e8df71cf66c3e04eb
liangguifeng <1476982312@qq.com>
2022-06-20 10:09:32 +0800

    feat: RealWorld project api proto init

---
+++ internal/server/grpc.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	v1 "realworld-go-kratos/api/realworld/v1"
+	"realworld-go-kratos/internal/conf"
+	"realworld-go-kratos/internal/service"
+)
+
+// NewGRPCServer new a gRPC server.
+func NewGRPCServer(c *conf.Server, realWorld *service.RealWorldService, logger log.Logger) *grpc.Server {
+	var opts = []grpc.ServerOption{
+		grpc.Middleware(
+			recovery.Recovery(),
+		),
+	}
+	if c.Grpc.Network != "" {
+		opts = append(opts, grpc.Network(c.Grpc.Network))
+	}
+	if c.Grpc.Addr != "" {
+		opts = append(opts, grpc.Address(c.Grpc.Addr))
+	}
+	if c.Grpc.Timeout != nil {
+		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
+	}
+	srv := grpc.NewServer(opts...)
+
+	v1.RegisterRealWorldServer(srv, realWorld)
+
+	return srv
+}
